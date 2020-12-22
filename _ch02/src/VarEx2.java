9bfd39879e626b3ea00a6f979f3ec55c4522cf24
Barkb <Barkb@DESKTOP-PH5CUAU>
2020-12-22 16:36:15 +0900

    "update"

---
+++ _ch02/src/VarEx2.java
@@ -0,0 +1,14 @@
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+		
+		final int score;
+		score = 100;
+//		score = 200; 라인단위 주석 ctrl + /
+		
+		System.out.println(score); 
+		// 지역변수는 읽기전에 반드시 초기화를 해야한다. 
+		
+	}
+}
